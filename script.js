9895dfa4fad0ccae154d2b44a3be2abf084f9f29
JanisPriede1 <jpriede48@gmail.com>
2022-04-19 15:12:45 +0300

    3
    3

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function nolasa(m1, m2, m3) {
-    const malas = document.getElementsByClassName("mala")
+    const malas = document.getElementsByClassName("mala");
     m1 = parseFloat(mala1.value);
     m2 = parseFloat(mala2.value);
     m3 = parseFloat(mala3.value);
-
     if (m1 > 0 && m2 > 0 && m3 > 0) {
         console.log({ m1, m2, m3 });
         return { m1, m2, m3 };
     } else {
-        console.log(false)
+        console.log(false);
         return false;
     }
 }
-
 function perimetrs(m1, m2, m3) {
     const m = nolasa();
     m1 = m.m1; m2 = m.m2; m3 = m.m3;
@@ -20,21 +18,62 @@ function perimetrs(m1, m2, m3) {
     const p = m1 + m2 + m3;
     console.log(p);
     return (p);
+
 }
+
 function pusper(m1, m2, m3) {
     const pusper = perimetrs(m1, m2, m3) / 2;
     console.log(pusper);
     return (pusper);
-
 }
+
 function laukums(m1, m2, m3) {
-    const pusper = perimetrs(m1, m2, m3) / 2;
-    let tlaukums = Math.sqrt(
+    const pusper = perimetrs(m1, m2, m3);
+    let laukums = Math.sqrt(
         pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
-    console.log(tlaukums)
-    return tlaukums;
+    console.log(laukums);
+    return laukums;
+}
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function rezultats() {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()) {
+        t = " Nekorekti ievaddati, trijstura malu vertibam ir jabut lielakam par 0 ";
+    } else {
+        if (irTrijsturis(m1, m2, m3) == false) {
+            t = " Trijsturis neeksiste, jo jebkuru 2 malu garumu sumai ir jabut lielakai par treso malu! ";
+        } else {
+            t = " Trijsturis ar malu garumiem " + m1 + "," + m2 + " un" + m3 + " eksiste."
+            if (m1 == m2 && m2 == m3) {
+                t += " Tas ir vienadmalu trijsturis."
+            } else {
+                if (m1 == m2 || m2 == m3 || m1 == m3) {
+
+
+                }
+            }
+            const p = perimetrs(m1, m2, m3);
+            const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+            t += " Perimetrs ir " + p + " un laukums ir " + s + ".";
+        }
+    }
+    console.log(t);
+    return t;
 }
 
-
-
-
+function izvadaTekstu() {
+    const teksts = rezultats();
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    raksti.innerHTML = teksts;
+    sakne.appendChild(raksti);
+}
